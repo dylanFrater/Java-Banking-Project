2a592b49c5c27bddc593d54bfd41c7681e913aa6
Mohammad <mohammadsalaar25@gmail.com>
2026-04-15 18:28:15 -0400

    Clean up application startup

--- src/main/java/com/bank/banking_app/BankingAppApplication.java
+++ src/main/java/com/bank/banking_app/BankingAppApplication.java
@@ -1,8 +1,5 @@
 package com.bank.banking_app;
 
-import com.bank.banking_app.model.User;
-import com.bank.banking_app.service.UserService;
-import com.bank.banking_app.util.AccountUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,22 +8,5 @@ public class BankingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
-
-		try {
-			User user = new User(
-					"jdoe2",
-					"password123",
-					"1234",
-					"John Doe",
-					AccountUtil.generateCardNumber(),
-					AccountUtil.generateCVC(),
-					AccountUtil.generateExpirationDate()
-			);
-
-			new UserService().createUser(user);
-			System.out.println("User created successfully.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
